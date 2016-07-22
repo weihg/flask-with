0b5be577fda109c2fbbc127407c5bf2d4fffde60
weihg <872482352@qq.cm>
2016-07-22 07:45:00 -0700

    branch

--- giterror.py
+++ giterror.py
@@ -9,3 +9,31 @@ git push -u origin master
 
 ---------------------
 git remote rm origni
+
+-------------------
+===branch dev===
+git checkout -b dev  ==>> git branch dev   git checkout dev
+
+git branch
+
+git add filename
+
+git commit -m "branch changes"
+
+git chackout master
+
+=== merge dev =>master===
+git merge dev
+
+===del dev==
+git branch -d dev
+
+git branch
+
+--------conclusion----------
+<1> view         ---- git branch
+<2> create branch --- git bracnh <name>
+<3> switch branch --- git checkout <name>
+<4>create + switch breach --git checkout -b <name>
+<5> merge branch  ------- git merge <name>
+<6> delete branch  ------ git branch -d <name>
